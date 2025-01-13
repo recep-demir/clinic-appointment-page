31f5fe7b470ca0a2a4b19de1df35731b6b09cc7f
recep-demir <demir.rp@gmail.com>
2025-01-13 13:48:14 +0100

    create patient area and css files

--- src/components/AppointmentList.jsx
+++ src/components/AppointmentList.jsx
@@ -1,16 +1,37 @@
 import React from 'react'
+import "../CSS/AppointmentList.css"
+import { MdOutlineCancel } from "react-icons/md";
+
+
+
 
 const AppointmentList = ({appointment}) => {
-  return (
-    <div>
-        {appointment.map((item)=>(
-            <h1>appointment</h1>
-        ))
-            
-        }
-        
+    return (
+        <div >
+          {appointment && appointment.length > 0 ? (
+            appointment.map((item) => (
+              <div className='patientlist' key={item.id}>
+                <div>
+                <h4 className='text-danger'>{item.patient}</h4>
+                <h5>{item.doctor}</h5>
+                </div>
+                <div>
+                <h4>{new Date(item.day).toLocaleDateString()}</h4>
+
+                </div>
+                <div className='mt-3'>
+                <MdOutlineCancel className='icon-red' />
+                </div>
+                
+                
+               
+              </div>
+            ))
+          ) : (
+            <p>No appointments available</p>
+          )}
         </div>
-  )
+      );
 }
 
 export default AppointmentList
